f8c36bd1747afc609476e993da49405a9462ea40
Christo Bezuidenhout <bez.christo@gmail.com>
2021-04-29 15:35:13 +0200

    Added provisional Gateway spec for Terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+provider "google-beta" {
+  project = var.project
+  version = "~> 3.48.0"
+}
+
+provider "google" {
+  project = var.project
+  version = "~> 3.65"
+}
+
 resource "google_storage_bucket" "bucket" {
   name = "unbapi_source_code"
 }
@@ -21,6 +31,33 @@ resource "google_cloudfunctions_function" "investecPay_function" {
   trigger_http          = true
   project               = var.project
   region                = "us-central1"
+}
+
+resource "google_api_gateway_api" "api_gw" {
+  provider = google-beta
+  api_id = "api-gw"
+}
+
+resource "google_api_gateway_api_config" "api_gw" {
+  provider = google-beta
+  api = google_api_gateway_api.api_gw.api_id
+  api_config_id = "config"
+
+  openapi_documents {
+    document {
+      path = "spec.yaml"
+      contents = filebase64("./src/apigateway/openapi.yaml")
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_api_gateway_gateway" "api_gw" {
+  provider = google-beta
+  api_config = google_api_gateway_api_config.api_gw.id
+  gateway_id = "api-gw"
 }
 
 # IAM entry for a single user to invoke the function
