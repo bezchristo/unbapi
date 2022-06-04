3eacc1c92f67bb00702213ccb116f7b6daf49140
Christo <c.bezuidenhout@betagy.com>
2022-06-04 11:26:16 +0200

    Updated to work with latest Investec website

--- README.md
+++ README.md
@@ -5,26 +5,28 @@ The following tutotial will set up an unofficial banking api service
 It makes use of the following services:
 * Cloud Functions
 * Gateway API
+* Taiko
+* Nodejs	
 
 To setup the unofficial api follow the tutorial on google cloud shell.
 
 [![Open in Cloud Shell](https://gstatic.com/cloudssh/images/open-btn.svg)](https://ssh.cloud.google.com/cloudshell/editor?cloudshell_git_repo=https%3A%2F%2Fgithub.com%2Fbezchristo%2Funbapi.git&cloudshell_print=cloud-shell-readme.txt&cloudshell_open_in_editor=main.tf&cloudshell_tutorial=tutorial.md&hl=en_GB&fromcloudshell=true&shellonly=false#id=I0_1588005425124&_gfid=I0_1588005425124&parent=https:%2F%2Fconsole.cloud.google.com)
 
-## Which banks are currently supported
+## Which banks are currently supported?
 * Investec - Personal
 
-## How to setup a bank
+## How to add more banks
 1. Create a folder under `\src\functions` with the Bank's name in the format `{{bank}}Pay`.
 2. Add an `index.js` and `package.json` file containing the taiko code to make the payment.
 3. Zip both files into a zip file named the same as the folder i.e. `investecPay.zip`.
-4. Edit the `openapi.yaml` file and add a new `path` for your bank.
+4. Edit the `\apigateway\openapi.yaml` file and add a new `path` for your bank.
 5. Edit the main.tf file and add the folowing:
    * Add a new `resource "google_storage_bucket_object"` for your bank.
    * Add a new `resource "google_cloudfunctions_function"` for your bank.
    * Add a new `resource "google_cloudfunctions_function_iam_member"` for your bank.
 6. Create a PR.
 
-Once the PR is merged I will run the terraform script to deploy the new bank.
+Once the PR is merged, run the terraform script to deploy the new bank.
 
 ## Issues
 If you run into any issues on the tutorial please raise an issue on github.
--- src/functions/investecPay/index.js
+++ src/functions/investecPay/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { openBrowser, goto, write, into, textBox, click, checkBox, dropDown, closeBrowser, waitFor, scrollDown } = require('taiko');
+import { openBrowser, goto, write, into, textBox, click, checkBox, dropDown, closeBrowser, waitFor, scrollDown } from 'taiko';
 /**
  * Publishes a message to a Cloud Pub/Sub Topic. *
  * @param {object} req Cloud Function request context.
@@ -8,9 +8,7 @@ const { openBrowser, goto, write, into, textBox, click, checkBox, dropDown, clos
  * @param {object} res Cloud Function response context.
  */
 
-exports.investecPay = async (req, res) => {  
-
-  console.log(`Making payment`);
+export async function investecPay(req, res) {  
 
    const data = req.body;
 
@@ -21,7 +19,7 @@ exports.investecPay = async (req, res) => {
         await write(data.password,into(textBox("Password")));
         await click("Log In");
         await waitFor("Make a payment", 60000);
-        await goto("https://login.secure.investec.com/io/payments-sa/payment-method/make-payment/onceoff", {waitForNavigation: false});	
+        await goto("https://login.secure.investec.com/pc-za/payments-sa/payment-method/make-payment/onceoff", {waitForNavigation: false});	
 
         await waitFor("Alert: Cellphone snatching", 60000);
 		await scrollDown(1000);
@@ -32,7 +30,7 @@ exports.investecPay = async (req, res) => {
         await dropDown("Bank").select(data.bank);
         await dropDown("Account type").select(data.accountType);
         await write(data.accountNumber,into(textBox("Account number")));
-        await write(data.description,into(textBox("My description")));
+        await write(data.description,into(textBox("My reference")));
         await write(data.reference,into(textBox("Their reference")));
         await write(data.amount,into(textBox("Amount")));
 
@@ -49,4 +47,4 @@ exports.investecPay = async (req, res) => {
     } finally {
         await closeBrowser();
     }  
-};
+}
